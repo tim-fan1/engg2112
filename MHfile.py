30933394b4249708945ef444121600faeb24cd7e
Tim Fan <tim.fan1@icloud.com>
2026-03-31 11:43:50 +1100

    second byebye idea 🥹

--- MHfile.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the user-provided dataset
-df = pd.read_csv('Mental Health dataset1.csv')
-
-# Display basic information
-print(df.info())
-print(df.head())
-print(df.columns)
