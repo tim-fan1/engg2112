f94019bbfbd55660d7d99b534778221cb8d53dd3
Meigo <rnoc0036@uni.sydney.edu.au>
2026-04-21 13:30:06 +1000

    unique postcodes

--- datasets/fuel/postcode_check.py
+++ datasets/fuel/postcode_check.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load your dataset
-df = pd.read_csv('datasets/fuel/fuelcheck_pricehistory_2026_02.csv')
+df = pd.read_csv('engg2112/datasets/fuel/fuelcheck_pricehistory_2026_02.csv')
 
 # Get a list of unique postcodes
 unique_postcodes = df['Postcode'].unique().tolist()
